ae9f1e201cd0a39aba50aa2e449c094bff288919
masamichi kurokawa <m-kurokawa@n-links.co.jp>
2022-04-04 16:04:54 +0900

    FIX_DirectoryChenage

--- api/routers/anime_vote_router.py
+++ api/routers/anime_vote_router.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import Session
 
 from database_setting import get_db
 from schemas import anime_vote_schema
-from database import anime_vote_crud
+from database import anime_vote_db
 
 router = APIRouter()
 
@@ -15,7 +15,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 @router.get("/animevote", response_model=List[anime_vote_schema.AnimeVoteBase])
 def list_vote(db: Session = Depends(get_db)):
-    return anime_vote_crud.get_vote_all(db)
+    return anime_vote_db.get_vote_all(db)
 
 
 @router.get("/animevote/ranking/{limit}{skip}", response_model=List[anime_vote_schema.AnimeVoteRank])
@@ -26,14 +26,14 @@ def get_specific_votes(db: Session = Depends(get_db),
                         ):
 
     if (filter is None):
-        return anime_vote_crud.get_vote_limit_skip_ranking(db,limit,skip)
+        return anime_vote_db.get_vote_limit_skip_ranking(db,limit,skip)
     else:
         filterSplit = filter.split(',')
         filterListToInt = [int(s) for s in filterSplit]
         print("kokoko")
         print(filterListToInt)
-        return anime_vote_crud.get_vote_limit_skip_filterlist_ranking(db,limit,skip,filterListToInt)
+        return anime_vote_db.get_vote_limit_skip_filterlist_ranking(db,limit,skip,filterListToInt)
 
 @router.post("/animevote/vote", response_model=anime_vote_schema.AnimeVoteBase)
 def create_vote(vote: anime_vote_schema.CreateVote,db: Session = Depends(get_db)):
-    return anime_vote_crud.create_vote(db=db,vote=vote)
+    return anime_vote_db.create_vote(db=db,vote=vote)
--- api/routers/login_router.py
+++ api/routers/login_router.py
@@ -6,23 +6,23 @@ from sqlalchemy.orm import Session
 from database_setting import get_db
 from models import user_model
 from schemas import login_schema
-from database import login_crud
+from database import login_db
 
 router = APIRouter()
 
 @router.post("/token", response_model=login_schema.Token)
 def login(form: OAuth2PasswordRequestForm = Depends(),db:Session = Depends(get_db)):
     """トークン発行"""
-    user = login_crud.authenticate(form.username, form.password,db)
-    return login_crud.create_tokens(user.user_id,db)
+    user = login_db.authenticate(form.username, form.password,db)
+    return login_db.create_tokens(user.user_id,db)
 
 @router.get("/refresh_token/", response_model=login_schema.Token)
-def refresh_token(current_user: user_model.User = Depends(login_crud.get_current_user_with_refresh_token)):
+def refresh_token(current_user: user_model.User = Depends(login_db.get_current_user_with_refresh_token)):
     """リフレッシュトークンでトークンを再取得"""
-    return login_crud.create_tokens(current_user.id)
+    return login_db.create_tokens(current_user.id)
 
 @router.get("/users/me/", response_model= login_schema.User)
-def read_users_me(current_user: user_model.User = Depends(login_crud.get_current_user),):
+def read_users_me(current_user: user_model.User = Depends(login_db.get_current_user),):
     """ログイン中のユーザーを取得"""
     return current_user
 
